185386a31fd3301ec8598b71bd60f3f629e16b8a
chris <chrispyprogrammer@gmail.com>
2018-10-21 08:47:16 +0200

    initial db  migration

--- src/status/models.py
+++ src/status/models.py
@@ -20,17 +20,17 @@ class StatusManager(models.Manager):
         return StatusQuerySet(self.model, using=self._db)
 
 
-# Create your models here.
+
 class Status(models.Model):
 
 	user		= models.ForeignKey(settings.AUTH_USER_MODEL)
 	content		= models.TextField(null=True, blank=True)
-	image		= models.imageField(upload_to=upload_status_image, null=True, blank=True)
+	image		= models.ImageField(upload_to=upload_status_image, null=True, blank=True)
 	update		= models.DateTimeField(auto_now=True)
 	timestamp	= models.DateTimeField(auto_now_add=True)
 
 
-	Objects	= StatusManager()
+	objects	= StatusManager()
 
 
 	def __str__(self):
