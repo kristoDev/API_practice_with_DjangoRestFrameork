ccbcd482e9b2c0e404b32efd19b4d61908654c39
chris <chrispyprogrammer@gmail.com>
2018-10-22 23:09:17 +0200

    readjusted virtualenv / added and initial customization of forms.py file

--- src/status/admin.py
+++ src/status/admin.py
@@ -2,8 +2,23 @@
 # from __future__ import unicode_literals
 
 from django.contrib import admin
+
+from .forms import StatusForm
 from .models import Status
 
 
 # Register your models here.
-admin.site.register(Status)
+
+class StatusAdmin(admin.ModelAdmin):
+	
+	list_display = [
+
+	'user',
+	'__str__',
+	'image'
+
+	] 
+
+	form = StatusForm
+
+admin.site.register(Status,StatusAdmin)
